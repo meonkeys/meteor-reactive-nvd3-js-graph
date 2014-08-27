a3f77421c870f06788f1cea3ecf34b1e20abe7eb
Adam Monsen <haircut@gmail.com>
2014-08-26 20:55:58 -0700

    use consistent indenting

--- test.js
+++ test.js
@@ -3,12 +3,12 @@ People = new Meteor.Collection("people");
 if (Meteor.isClient) {
   Template.hello.rendered = function() {
     var chart = nv.models.lineChart()
-                  .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                  .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                  .transitionDuration(350)  //how fast do you want the lines to transition?
-                  .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                  .showYAxis(true)        //Show the y-axis
-                  .showXAxis(true)        //Show the x-axis
+      .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+      .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+      .transitionDuration(350)  //how fast do you want the lines to transition?
+      .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+      .showYAxis(true)        //Show the y-axis
+      .showXAxis(true)        //Show the x-axis
     ;
 
     nv.addGraph(function() {
@@ -22,10 +22,10 @@ if (Meteor.isClient) {
     });
 
     Deps.autorun(function () {
-        d3.select('#chart svg').datum(
-          [{ values: People.find().fetch(), key: 'Age' }]
-        ).call(chart);
-        chart.update();
+      d3.select('#chart svg').datum(
+        [{ values: People.find().fetch(), key: 'Age' }]
+      ).call(chart);
+      chart.update();
     });
   };
 }
